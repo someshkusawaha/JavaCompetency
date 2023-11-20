468c633a561ec50948c5552d3700c1416768b594
somesh Kusawaha <someshKusawah.sk@gmail.com>
2023-11-20 15:47:51 +0530

    Calcultor : Added logback-spring.xml file.

--- Calculator/src/main/java/com/javacompentency/Calculator/controller/CalculatorController.java
+++ Calculator/src/main/java/com/javacompentency/Calculator/controller/CalculatorController.java
@@ -21,49 +21,49 @@ public class CalculatorController {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CalculatorController.class);
 
-	@GetMapping("/calculatorapi/v1/addition")
+	@GetMapping("/v1/addition")
 	public Map<String,String> addition(@RequestParam Integer number1,@RequestParam Integer number2 ){
 		LOG.info("CalculatorController : addition Start");
 		return calculatorService.addition(number1, number2);
 	}
 	
-	@GetMapping("/calculatorapi/v1/subtraction")
+	@GetMapping("/v1/subtraction")
 	public Map<String,String> subtraction(@RequestParam Integer number1,@RequestParam Integer number2 ){
 		LOG.info("CalculatorController : subtraction Start");
 		return calculatorService.subtraction(number1, number2);
 	}
 	
-	@GetMapping("/calculatorapi/v1/multiplication")
+	@GetMapping("/v1/multiplication")
 	public Map<String,String> multiplication(@RequestParam Integer number1,@RequestParam Integer number2 ){
 		LOG.info("CalculatorController : multiplication Start");
 		return calculatorService.multiplication(number1, number2);
 	}
 	
-	@GetMapping("/calculatorapi/v1/division")
+	@GetMapping("/v1/division")
 	public Map<String,String> division(@RequestParam Integer number1,@RequestParam Integer number2 ){
 		LOG.info("CalculatorController : division Start");
 		return calculatorService.division(number1, number2);
 	}
 	
-	@GetMapping("/calculatorapi/v1/square/{number}")
+	@GetMapping("/v1/square/{number}")
 	public Map<String,String> square(@PathVariable Integer number){
 		LOG.info("CalculatorController : square Start");
 		return calculatorService.square(number);
 	}
 	
-	@GetMapping("/calculatorapi/v1/squareroot/{number}")
+	@GetMapping("/v1/squareroot/{number}")
 	public Map<String,String> squareroot(@PathVariable Integer number){
 		LOG.info("CalculatorController : squareroot Start");
 		return calculatorService.squareroot(number);
 	}
 	
-	@GetMapping("/calculatorapi/v1/factorial/{number}")
+	@GetMapping("/v1/factorial/{number}")
 	public Map<String,String> factorial(@PathVariable Integer number){
 		LOG.info("CalculatorController : factorial Start");
 		return calculatorService.factorial(number);
 	}
 	
-	@GetMapping("/calculatorapi/v1/minmax")
+	@GetMapping("/v1/minmax")
 	public Map<String,String> minmax(@RequestBody MinMax minmax){
 		LOG.info("CalculatorController : minmax Start");
 		return calculatorService.minmax(minmax);
